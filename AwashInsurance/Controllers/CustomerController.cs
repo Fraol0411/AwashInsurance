764916511b226cc879bcb9fb6fd4db663b61c620
Fraol0411 <Fraol0411@gmail.com>
2025-05-09 14:39:12 +0300

    clear arvhitecture

---
+++ AwashInsurance/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AwashInsurance.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Modify()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Inquire()
+        {
+            return View();
+        }
+    }
+}
